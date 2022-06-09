af506064d6d32604ac5fe81dc2b17f46bb288255
NeranjanK <neranjan.kuppuswamy@gmail.com>
2022-06-09 00:57:55 -0700

    small changes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING!')
+})
+
 app.use('/jobs', jobRoutes);
 app.use('/user', userRoutes);
 
